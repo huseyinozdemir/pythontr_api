003cae217a3c4e180dc89d9de59cbb9c7bebd08a
Hüseyin Özdemir <husonet@pythongo.local>
2019-12-22 03:42:10 +0300

    Added custom model

--- README.md
+++ README.md
@@ -12,5 +12,7 @@ Development Notes:
 Start project on docker-compose
 
 Tips:
-* docker-compose run app sh -c "django-admin.py startproject app ." # Start Project
+* docker-compose run app sh -c "django-admin.py startproject app ." # Create project
 * docker-compose run app sh -c "python manage.py test && flake8" # Test
+* docker-compose run app sh -c "python manage.py startapp core" # Create core
+* docker-compose run app sh -c "python manage.py makemigrations" # Make migrations
---
+++ app/core/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                       PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
