ad16f36ebe94eeedb7e69d711df864a32a4510af
Hüseyin Özdemir <husonet@pythongo.local>
2020-01-18 01:49:06 +0300

    Added model article

--- app/core/models.py
+++ app/core/models.py
@@ -100,3 +100,32 @@ class Category(models.Model):
         full_path = ' / '.join(full_path[::-1])
 
         return full_path
+
+
+class Article(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(
+        Category,
+        related_name='article',
+    )
+    title = models.CharField(max_length=255, unique=True)
+    title_h1 = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    content = models.TextField(blank=True)
+    read_count = models.IntegerField(default=0)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,
+        null=True, blank=True
+    )
+    is_active = models.BooleanField(default=False)
+    is_approval = models.BooleanField(default=False)
+    approval_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='approval_user',
+        on_delete=models.SET_NULL,
+        null=True, blank=True
+    )
+    is_delete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
---
+++ app/core/tests/test_article_model.py
@@ -0,0 +1,63 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
+
+from core.models import Category, Article
+
+
+def test_user(email='testarticle@hotmail.com', password='123qwe'):
+    return get_user_model().objects.create_user(email, password)
+
+
+def test_user_other(email='testarticleother@hotmail.com', password='123qwe'):
+    return get_user_model().objects.create_user(email, password)
+
+
+def test_category(name='Python Article Programlama',
+                  short_name='python article'):
+    return Category.objects.create(
+        user=test_user(),
+        name=name,
+        short_name=short_name,
+    )
+
+
+class ModelTests(TestCase):
+
+    def test_article_str(self):
+        category = test_category()
+        user = test_user_other()
+        article = Article.objects.create(
+            user=user,
+            title='Python programlama',
+            description='python',
+            approval_user=user,
+        )
+        article.categories.add(category)
+
+        self.assertEqual(str(article), article.title)
+
+    def test_article_title_uniqe_check(self):
+        Article.objects.create(
+            user=test_user(),
+            title='Python',
+            description='python',
+            approval_user=test_user_other(),
+        )
+        with self.assertRaises(IntegrityError):
+            Article.objects.create(
+                user=test_user(),
+                title='Python',
+                description='python2',
+                approval_user=test_user_other(),
+            )
+
+    def test_article_category_type_error(self):
+        with self.assertRaises(TypeError):
+            Article.objects.create(
+                user=test_user_other(),
+                category=None,
+                title='Python',
+                description='python',
+                approval_user=test_user(),
+            )
