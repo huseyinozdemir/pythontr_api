387489ca356003b2301314f7cffd14b84b9fb1f3
Hüseyin ÖZDEMİR <husonet@pythongo.local>
2020-04-21 19:49:40 +0300

    Test status changes

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -3,8 +3,8 @@
 echo "Running pre-commit hook"
 ./scripts/run-django-tests.sh
 
-# $? son komut cikis degerini saklar
+# $? son komut sonlanma degerini saklar
 if [ $? -ne 0 ]; then
- echo "Unit Tests or Flake 8 must pass before commit!"
+ echo "Unit Tests or Flake8 must pass before commit!"
  exit 1
 fi
