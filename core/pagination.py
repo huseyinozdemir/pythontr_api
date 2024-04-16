a3d1803cc42c5ede532fefa3998349e7ded84982
Hüseyin Özdemir <huseyin@pythontr.com>
2024-04-16 14:11:35 +0300

    PYT-18: Modified pagination

--- core/pagination.py
+++ core/pagination.py
@@ -1,9 +1,12 @@
+import re
+
 from rest_framework import pagination
 from rest_framework.response import Response
 
 
 class CustomPagination(pagination.PageNumberPagination):
     def get_paginated_response(self, data):
+        current_page_number = self.page.number
         next_link = self.get_next_link()
         previous_link = self.get_previous_link()
 
@@ -12,13 +15,20 @@ class CustomPagination(pagination.PageNumberPagination):
         if previous_link:
             previous_link = '/{}'.format(previous_link.split('/', 3)[-1])
 
+        first_page = re.sub(
+            r'\?page=\d+', '', previous_link) if previous_link else None
+        last_page = re.sub(
+            r'\?page=\d+', '?page={}'.format(
+                self.page.paginator.num_pages
+            ), next_link) if next_link else None
+
         return Response({
+            'count': self.page.paginator.count,
+            'first_page': first_page,
+            'last_page': last_page,
             'next': next_link,
             'previous': previous_link,
-            'count': self.page.paginator.count,
+            'last_page_number': self.page.paginator.num_pages,
+            'current_page': current_page_number,
             'results': data,
-            'incomplete_results': (
-                self.page.paginator.count > self.page.paginator.num_pages *
-                self.page_size
-            ),
         })
