8c4c30b6a60860abcab0297236d1e28ec2810b55
Hüseyin Özdemir <huseyin@pythontr.com>
2024-12-10 00:18:11 +0300

    PYT-26: Django Upgrade 3.x to 4.2.16

--- recipe/serializers/serializer_comment.py
+++ recipe/serializers/serializer_comment.py
@@ -2,7 +2,7 @@ import sys
 import requests
 
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from rest_framework import serializers
 
